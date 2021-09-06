814f8fde62876628618fbbba682143ba06a0e88f
gary1327 <54121714+gary1327@users.noreply.github.com>
2021-09-06 17:16:31 -0400

    file cleanup

--- game.cpp
+++ game.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 #include <ctime>
 #include <cstdlib>
-
+#include <Windows.h>
 
 enum
 {
@@ -23,23 +23,7 @@ enum
     LARGE // [30, INF)
 };
 
-enum
-{
-    GRASS,
-    BERRIES,
-    FLOWERS,
-    SEEDS,
-    STEM,
-    CAFEFOOD,
-};
 
-enum
-{
-    GENTLE,
-    FIRM,
-    HARSH,
-    EXTREME
-};
 
 const int MAXNAMENUMBER = 16;
 const int MAXGOOSENUMBER = 200;
@@ -50,6 +34,9 @@ const int SPEED_Y_UPPER = 3;
 const int SPEED_Y_LOWER = -3;
 const int SCREENX = 80;
 const int SCREENY = 300;
+const int INVALIDGOOSEINDEX = -1;
+
+void drawGoose();
 
 
 typedef struct 
@@ -76,12 +63,32 @@ Goose createRandomGoose(const char* name)
 {
     Goose goose;
     strcpy_s(goose.name, name);
-    goose.level = 18;
-    goose.exp = 0.0f;
     goose.health = 3;
 
+    goose.size = TINY;
+    goose.level = 1;
+    goose.exp = float(rand() % 1024);
+    while (goose.exp >= 0.5f * goose.level * goose.level)
+    {
+        goose.exp -= 0.5f * goose.level * goose.level;
+        ++goose.level;
+        goose.health = 2 * goose.level + 1;
+        if (goose.level >= 30)
+        {
+            goose.size = LARGE;
+        }
+        else if (goose.level >= 18)
+        {
+            goose.size = MEDIUM;
+        }
+        else if (goose.level >= 8)
+        {
+            goose.size = SMALL;
+        }
+        //else its tiny 
+    }
 
-    goose.size = MEDIUM;
+
     goose.breed = rand() % 6;
     goose.vector_x = rand() % 4;
     goose.vector_y = rand() % 4;
@@ -140,7 +147,7 @@ void addGoose(Goose goose)
     }
 }
 
-int findGoose(char* name)
+int findGoose(const char* name)
 {
     for (int i = 0; i < MAXGOOSENUMBER; ++i)
     {
@@ -149,7 +156,7 @@ int findGoose(char* name)
             return i;
         }
     }
-    return -1;
+    return INVALIDGOOSEINDEX;
 }
 
 
@@ -177,7 +184,26 @@ void levelUp(int gooseIndex)
     }
 }
 
-float foodValue[6] = { 1.0f, 5.0f, 3.0f, 2.0f, 1.0f, 10.0f };
+
+
+
+
+//FEED FUNCTIONS
+
+enum
+{
+    GRASS,
+    BERRIES,
+    FLOWERS,
+    SEEDS,
+    STEM,
+    CAFEFOOD,
+};
+const int MAXFOODNUMBER = 6;
+int money = 69;
+float foodValue[MAXFOODNUMBER] = { 3.0f, 15.0f, 9.0f, 6.0f, 1.5f, 30.0f };
+const char* foodName[MAXFOODNUMBER] = { "Grass", "Berries", "Flowers", "Seeds", "Stem", "Cafefood" };
+int foodPrice[MAXFOODNUMBER] = { 10, 45, 28, 19, 6, 85 };
 void feed(int gooseIndex, int foodIndex)
 {
     float value = foodValue[foodIndex];
@@ -187,9 +213,23 @@ void feed(int gooseIndex, int foodIndex)
 }
 
 
-float petExpGain[4] = { 1.0f, 5.0f, 12.0f, 30.0f };
-float petSuccessChance[4] = { 0.85f, 0.65f, 0.25f, 0.01f };
-int petHealthPenalty[4] = { 1, 3, 7, 12 };
+
+
+
+//PET FUNCTIONS
+enum
+{
+    GENTLE,
+    FIRM,
+    HARSH,
+    EXTREME
+};
+const int MAXPETNUMBER = 4;
+float petExpGain[MAXPETNUMBER] = { 1.0f, 5.0f, 12.0f, 30.0f };
+float petSuccessChance[MAXPETNUMBER] = { 0.85f, 0.65f, 0.25f, 0.01f };
+int petHealthPenalty[MAXPETNUMBER] = { 1, 3, 7, 12 };
+const char* petName[MAXPETNUMBER] = {"gentle", "firm", "harsh", "extreme"};
+
 
 void pet(int gooseIndex, int power)
 {
@@ -205,6 +245,9 @@ void pet(int gooseIndex, int power)
     }
 }
 
+
+//MATING FUNCTION
+const float MATINGRATE = 0.75;
 void mating()
 {
     for (int i = 0; i < MAXGOOSENUMBER; ++i)
@@ -227,8 +270,13 @@ void mating()
             int distance = sqrtf(float(x_diff * x_diff + y_diff * y_diff));
             if (distance <= MATING_DISTANCE)
             {
-                Goose childGoose = createNewBornGoose(i, j);
-                addGoose(childGoose);
+                float mate_chance = double(rand()) / double(RAND_MAX);
+                if (mate_chance < MATINGRATE)
+                {
+
+                    Goose childGoose = createNewBornGoose(i, j);
+                    addGoose(childGoose);
+                }
             }
         }
     }
@@ -350,9 +398,6 @@ char screen[SCREENX][SCREENY];
 
 void drawGoose()
 {
-
-
-
     //clear the old screen
     for (int x = 0; x < SCREENX; ++x)
     {
@@ -360,14 +405,12 @@ void drawGoose()
         {
             screen[x][y] = ' ';
         }
-        screen[x][SCREENY - 1] = '\n';
     }
 
     for (int i = 0; i < MAXGOOSENUMBER; ++i)
     {
         //skip the dead gooseo
         if (gooseList[i].health <= 0) continue;
-
 
         //printing the name
         for (int str_index = 0, x = gooseList[i].x, y = gooseList[i].y; /*No condition*/; ++str_index, ++y)
@@ -404,32 +447,187 @@ void drawGoose()
         }
     }
 
+    //add the new line
+    for (int x = 0; x < SCREENX; ++x)
+    {
+        screen[x][SCREENY - 1] = '\n';
+    }
+
     //print the screen
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n%s", screen);
+    system("cls");
+    printf("%s\n\nGoose Profiles:\n", screen);
+    for (int i = 0; i < MAXGOOSENUMBER; ++i)
+    {
+        //skip the dead goose
+        if (gooseList[i].health <= 0) continue;
+
+        printf("Name: %16s    Health: %3i    Level: %2i    EXP: %.2f/%.2f\n", gooseList[i].name, gooseList[i].health, gooseList[i].level, gooseList[i].exp, 0.5f * gooseList[i].level * gooseList[i].level);
+    }
+    printf("\nBank: %i\n", money);
 }
+
+
+void feedMenu(int gooseIndex)
+{
+
+    while (true)
+    {
+        printf("Welcome to the uw cafe\n");
+        for (int i = 0; i < MAXFOODNUMBER; i++)
+        {
+            printf("%i %s %.2f exp %i$\n", i + 1, foodName[i], foodValue[i], foodPrice[i]);
+        }
+        printf("%i quit\n", MAXFOODNUMBER + 1);
+
+        int options;
+        scanf_s("%i", &options);
+        options -= 1;
+
+        if (options == MAXFOODNUMBER) break;
+
+        int cost = foodValue[options];
+        if (money < cost)
+        {
+            printf("hahahahah u broke\a\n");
+        }
+        else
+        {
+            money -= cost;
+            feed(gooseIndex, options);
+            move();
+            drawGoose();
+        }
+    }
+}
+
+void petMenu(int gooseIndex)
+{
+    while (true)
+    {
+        printf("%s looks like he/she really wants to get petted. Pet him/her?\n", gooseList[gooseIndex].name);
+
+        for (int i = 0; i < MAXPETNUMBER; ++i)
+        {
+            printf("%i-%s\n", i+1, petName[i]);
+        }
+        printf("%i-quit\n", MAXPETNUMBER + 1);
+
+
+        int options;
+        scanf_s("%i", &options);
+        options -= 1;
+
+        if (options == MAXPETNUMBER) break;
+
+        pet(gooseIndex, options);
+        move();
+        drawGoose();
+
+        if (gooseList[gooseIndex].health <= 0)
+        {
+            printf("AHHOOH, you murdered %s by accident\n", gooseList[gooseIndex].name);
+            break;
+        }
+    }
+}
+
+void gooseMenu(int gooseIndex)
+{
+    while (true)
+    {
+        int options;
+        printf("1-feed\n2-pet\n3-quit\n");
+        scanf_s("%i", &options);
+        if (options == 1)
+        {
+            feedMenu(gooseIndex);
+        }
+        else if (options == 2)
+        {
+            petMenu(gooseIndex);
+        }
+        else
+        {
+            break;
+        }
+
+        //check if the player killed the goose
+        if (gooseList[gooseIndex].health <= 0) break;
+    }
+   
+}
+
+
+void gameMenu()
+{
+    drawGoose();
+    while (true)
+    {
+        int options;
+        printf("1-select goose\n2-skip to next day\n3-quit\n");
+        scanf_s("%i", &options);
+        if (options == 1)
+        {
+            char gooseName[MAXNAMENUMBER];
+            printf("Enter the goose name:\n");
+            scanf_s("%s", &gooseName[0], MAXNAMENUMBER);
+
+            int gooseIndex = findGoose(&gooseName[0]);
+            if (gooseIndex != INVALIDGOOSEINDEX)
+            {
+                gooseMenu(gooseIndex);
+            }
+            else
+            {
+                printf("cannot find goose because goose does not exist or is dead\n");
+            }
+        }
+        else if (options == 2)
+        {
+            money += rand() % 15;
+
+            for (int i = 0; i < 24; ++i)
+            {
+                move();
+                mating();
+                drawGoose();
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+}
+
 
 int main (void)
 {
     //initialize the radom seed with the current time
     srand(time(NULL));
  
-    
-    
-    for (int i = 0; i < 2; ++i)
-    {
-        Goose ryan = createRandomGoose("AAAAAAAA");
-        addGoose(ryan);
-        ryan = createRandomGoose("BBBBBBBB");
-        addGoose(ryan);
-    }
 
-    while (true)
-    {
-        drawGoose();
-        move();
-        mating();
+    Goose g = createRandomGoose("MIKE");
+    addGoose(g);
+    g = createRandomGoose("MICHAEL");
+    addGoose(g);
+    g = createRandomGoose("JORDON");
+    addGoose(g);
+    g = createRandomGoose("TRUMP");
+    addGoose(g);
+    g = createRandomGoose("DONALD");
+    addGoose(g);
+    g = createRandomGoose("TRUDEAU");
+    addGoose(g);
+    g = createRandomGoose("POTATO");
+    addGoose(g);
+    g = createRandomGoose("AARON");
+    addGoose(g);
+    g = createRandomGoose("COOPER");
+    addGoose(g);
 
-        getchar();
-    }
+
+    gameMenu();
 }
 
